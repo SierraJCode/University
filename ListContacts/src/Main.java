ba799ef3322c34a580bc4668528fdba9a823d35c
Jesus Sierra <sierraja06@gmail.com>
2023-11-25 19:35:08 -0500

    Cambiando nombre de la clase main

--- ListContacts/src/Main.java
+++ ListContacts/src/Main.java
@@ -1,5 +1,5 @@
 
-public class Principal {
+public class Main {
     public static void main(String [] args){
         FormListContact newListContact = new FormListContact();
         newListContact.setVisible(true);
